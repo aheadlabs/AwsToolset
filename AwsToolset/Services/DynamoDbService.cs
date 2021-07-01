b358998e189856b31fabb30fc5870ab692c4063e
Alberto Carbonell Rubio <alberto.carbonell@aheadlabs.com>
2021-07-01 12:54:11 +0200

    Fixing some problems on dynamodb service

--- AwsToolset/Services/DynamoDbService.cs
+++ AwsToolset/Services/DynamoDbService.cs
@@ -34,7 +34,7 @@ namespace AwsToolset.Services
             {
                 _endPoint = value;
                 // Create the DynamoDb config
-                _config = new AmazonDynamoDBConfig { ServiceURL = _endPoint, RegionEndpoint = _regionEndpoint ?? RegionEndpoint.EUWest1 };
+                _config = new AmazonDynamoDBConfig { ServiceURL = _endPoint };
                 // Use the config to feed the client
                 _client = new AmazonDynamoDBClient(_config);
             }
@@ -43,11 +43,22 @@ namespace AwsToolset.Services
         /// <inheritdoc />
         public string Region
         {
-            set => _regionEndpoint = RegionEndpoint.GetBySystemName(value);
+            set
+            {
+                _regionEndpoint = RegionEndpoint.GetBySystemName(value);
+                // Create the DynamoDb config
+                _config = new AmazonDynamoDBConfig { RegionEndpoint = _regionEndpoint };
+                // Use the config to feed the client
+                _client = new AmazonDynamoDBClient(_config);
+            }
         }
 
         /// <inheritdoc />
-        public string Table { get => _tableEnvironment.TableName; set => _tableEnvironment = Amazon.DynamoDBv2.DocumentModel.Table.LoadTable(_client, value); }
+        public string Table
+        {
+            get => _tableEnvironment.TableName; 
+            set => _tableEnvironment = Amazon.DynamoDBv2.DocumentModel.Table.LoadTable(_client, value);
+        }
 
         #endregion
 
@@ -59,12 +70,12 @@ namespace AwsToolset.Services
             string jsonText = JsonSerializer.Serialize(inObject);
             var item = Document.FromJson(jsonText);
 
-            var putItemConfig = new PutItemOperationConfig
+            var putItemConfig = new UpdateItemOperationConfig()
             {
                 ReturnValues = ReturnValues.AllNewAttributes
             }; 
 
-            Document operationResult = _tableEnvironment.PutItemAsync(item, putItemConfig).Result;
+            Document operationResult = _tableEnvironment.UpdateItemAsync(item, putItemConfig).Result;
 
             TOut result = JsonSerializer.Deserialize<TOut>(operationResult.ToJson());
 
@@ -85,12 +96,14 @@ namespace AwsToolset.Services
 
             var updateItemConfig = new UpdateItemOperationConfig
             {
-                ReturnValues = ReturnValues.AllNewAttributes
+                ReturnValues = ReturnValues.UpdatedNewAttributes
             }; 
 
             Document operationResult = _tableEnvironment.UpdateItemAsync(item, updateItemConfig).Result;
+            
 
             TOut result = JsonSerializer.Deserialize<TOut>(operationResult.ToJson());
+            
 
             return result;
         }
