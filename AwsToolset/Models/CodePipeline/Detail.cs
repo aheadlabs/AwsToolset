69c70591bb5c3befe32ee644f7930b91424ccda3
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-07-12 17:46:06 +0200

    Added model for CodePipeline notifications.

---
+++ AwsToolset/Models/CodePipeline/Detail.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace AwsToolset.Models.CodePipeline
+{
+	public class Detail
+	{
+		public string Pipeline { get; set; }
+		[JsonProperty(PropertyName = "execution-id")]
+		public string ExecutionId { get; set; }
+		public string Stage { get; set; }
+		[JsonProperty(PropertyName = "execution-result")]
+		public ExecutionResult ExecutionResult { get; set; }
+		public string Action { get; set; }
+		public string State { get; set; }
+		public string Region { get; set; }
+		public Type Type { get; set; }
+		public float Version { get; set; }
+	}
+}
---
+++ AwsToolset/Models/CodePipeline/Type.cs
@@ -0,0 +1,10 @@
+﻿namespace AwsToolset.Models.CodePipeline
+{
+	public class Type
+	{
+		public string Owner { get; set; }
+		public string Provider { get; set; }
+		public string Category { get; set; }
+		public string Version { get; set; }
+	}
+}
