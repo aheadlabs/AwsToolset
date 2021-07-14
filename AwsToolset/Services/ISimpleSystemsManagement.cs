d2a0097489a9ee39e814c857d2e7aa31e5e43db9
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-07-14 23:47:49 +0200

    Added support for AWS SSM (Simple Systems Management). Updated dependencies.

---
+++ AwsToolset/Services/ISimpleSystemsManagement.cs
@@ -0,0 +1,23 @@
+﻿using Amazon;
+using System.Threading.Tasks;
+
+namespace AwsToolset.Services
+{
+	/// <summary>
+	/// Manages AWS systems via AWS SSM (Simple Systems Management)
+	/// </summary>
+	public interface ISimpleSystemsManagement
+	{
+		/// <summary>
+		/// AWS region endpoint
+		/// </summary>
+		RegionEndpoint RegionEndpoint { get; set; }
+
+		/// <summary>
+		/// Gets a string parameter given its name
+		/// </summary>
+		/// <param name="name">Parameter name</param>
+		/// <returns>Parameter string value</returns>
+		Task<string> GetStringParameter(string name);
+	}
+}
